6268e429934ad380f26c1530afd3fe4466c5af20
Jonathan Boydens <jonathan_boydens@hotmail.com>
2018-05-03 16:55:15 +0200

    project setup + first method tested

---
+++ PE1.BoydensJ.Lib/ReeksService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PE1.BoydensJ.Lib
+{
+    public class ReeksService
+    {
+        public IEnumerable<int> GeefReeks(int min, int max)
+        {
+            IList<int> reeks = new List<int>();
+            for (int i = min; i <= max; i++)
+            {
+                reeks.Add(i);
+            }
+
+            foreach (int n in reeks)
+                yield return n;
+        }
+    }
+}
---
+++ PE1.BoydensJ.TestCsl/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PE1.BoydensJ.Lib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PE1.BoydensJ.TestCsl
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IList<string> outputStrs = new List<string>();
+
+            var x = new ReeksService();
+            var allNumbers = x.GeefReeks(10, 20);
+            foreach(int v in allNumbers)
+            {
+                outputStrs.Add(v.ToString());
+            }
+            Print(outputStrs);
+            
+            Console.ReadKey();
+        }
+
+        static void Print(IEnumerable<string> strs)
+        {
+            foreach (var s in strs)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
