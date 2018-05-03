7e8bd10784273ce5f650f548422e2b8edb43dec5
Jonathan Boydens <jonathan_boydens@hotmail.com>
2018-05-03 16:01:32 +0200

    Add project files.

---
+++ PE1.BoydensJ.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PE1.BoydensJ.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
