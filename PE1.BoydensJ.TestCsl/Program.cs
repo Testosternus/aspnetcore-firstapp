27f595fc50f15607cc9cae9fb0a924feeaedfa35
Jonathan Boydens <jonathan_boydens@hotmail.com>
2018-05-06 11:27:18 +0200

    prep

--- PE1.BoydensJ.TestCsl/Program.cs
+++ PE1.BoydensJ.TestCsl/Program.cs
@@ -13,24 +13,25 @@ namespace PE1.BoydensJ.TestCsl
         public static void Main(string[] args)
         {
 
-            Task getNormal = StringConv("Test", TekstMode.Normal);
+            Task getNormal = StringConv("Hello Async!", TekstMode.Normal);
             getNormal.Wait();
 
-            Task getReverse = StringConv("Test", TekstMode.Reverse);
+            Task getReverse = StringConv("Hello Async!", TekstMode.Reverse);
             getReverse.Wait();
 
-            Task getAscii = StringConv("Test", TekstMode.Ascii);
+            Task getAscii = StringConv("Hello Async!", TekstMode.Ascii);
             getAscii.Wait();
+
             IList<string> outputStrs = new List<string>();
 
             var allNumbers = x.GeefReeks(10, 20);
             outputStrs.Add("All numbers from min to max:");
-            foreach(int a in allNumbers)
+            foreach (int a in allNumbers)
                 outputStrs.Add(a.ToString());
 
             var evenNumbers = x.GeefReeksEven(10, 20);
             outputStrs.Add("All even from min to max:");
-            foreach(int e in evenNumbers)
+            foreach (int e in evenNumbers)
                 outputStrs.Add(e.ToString());
 
             var primes = x.GeefPriemGetallen(10, 20);
@@ -39,7 +40,7 @@ namespace PE1.BoydensJ.TestCsl
                 outputStrs.Add(e.ToString());
 
             Print(outputStrs);
-            
+
             Console.ReadKey();
         }
 
@@ -52,7 +53,7 @@ namespace PE1.BoydensJ.TestCsl
         static async Task StringConv(string input, TekstMode m)
         {
             var resp = await y.GetTekst(input, m);
-            Console.WriteLine($"Input: {input} \r\n Output: {resp}");
+            Console.WriteLine($"Converting: {input} \r\n {resp}");
         }
     }
 }
